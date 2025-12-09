7d5f651ad0f9d63a2bb8597d9bd3a685e3618934
HoQuangDong <rumgaming321@gmail.com>
2025-12-09 14:11:49 +0700

    [FIX] Update UI

--- DoAnStudentManager/DoAnStudentManager/FormThongKe.Designer.cs
+++ DoAnStudentManager/DoAnStudentManager/FormThongKe.Designer.cs
@@ -33,9 +33,10 @@
             // 
             // FormThongKe
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(560, 305);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "FormThongKe";
             Text = "FormThongKe";
             ResumeLayout(false);
